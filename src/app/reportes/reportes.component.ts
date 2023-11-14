fab68ab701ffed77b1c1a5173da0379d68fac427
Braulio <villegasbraulio7@gmail.com>
2023-11-14 11:48:02 -0300

    reporte hardcodeado

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -69,11 +69,11 @@ export class ReportesComponent implements OnInit {
         const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
         this.basicDatahard = {
-            labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+            labels: ['Crema Depilatoria', 'Esmalte', 'Acondicionador', 'Mascarilla'],
             datasets: [
                 {
-                    label: 'Sales',
-                    data: [540, 325, 702, 620],
+                    label: 'Ventas de productos',
+                    data: [5, 3, 8, 10],
                     backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
                     borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
                     borderWidth: 1
