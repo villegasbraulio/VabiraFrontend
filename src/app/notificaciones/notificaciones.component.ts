50230bb83a38e14e64cf89c13d8be07c003daf16
Pancho Arana <panchoarana2014@gmail.com>
2024-01-31 14:31:24 -0300

    update

--- src/app/notificaciones/notificaciones.component.ts
+++ src/app/notificaciones/notificaciones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
 import { NotificacionesService } from './notificaciones.service';
 import { DatePipe } from '@angular/common';
 import { MessageService } from 'primeng/api';
@@ -12,6 +12,10 @@ import { ProveedorService } from '../proveedor/proveedor.service';
   styleUrls: ['./notificaciones.component.css']
 })
 export class NotificacionesComponent implements OnInit {
+  @Input() visible: boolean = true;
+  @Input() showButton: boolean = false;
+  @Input() customMessage: string = '';
+  @Output() buttonClick = new EventEmitter<void>();
 
   alerts: any[] = [];
   userClientId: number = 0; // Esto asegura que userClientId sea de tipo number
@@ -19,14 +23,16 @@ export class NotificacionesComponent implements OnInit {
   supplier: any;
   clienteIdFound: any;
   supplierIdFound: any;
+  showCustomButton: boolean = false;
+  customMessageContent: string = '';
+  currentTurno: any;
+  fecha: any;
 
 
   constructor(private notificacionesService: NotificacionesService,
     private datePipe: DatePipe,
     private messageService: MessageService,
-    private userService: UserService,
-    private clienteService: ClienteService,
-    private proveedorService: ProveedorService) { }
+    private userService: UserService,) { }
 
   ngOnInit(): void {
     this.userService.obtenerPerfil().subscribe(profile => {
@@ -64,97 +70,68 @@ export class NotificacionesComponent implements OnInit {
       for (const alert of alerts) {
         // Verificar si el cliente de la alerta coincide con el cliente del usuario
         if (alert.turn.client.id === this.clienteIdFound) {
-          for (const alert of alerts) {
-            // Verificar si el cliente de la alerta coincide con el cliente del usuario
-            if (alert.turn.client.id === this.clienteIdFound) {
-              console.log('ID del cliente de la alerta:', alert.turn.client.id);
-              console.log('ID de la alerta:', alert.id);
-              console.log('Nombre de la alerta:', alert.name);
-              console.log('Descripción de la alerta:', alert.description);
-              console.log('Fecha de inicio del turno:', alert.turn.dateFrom);
-              console.log('Fecha de fin del turno:', alert.turn.dateTo);
+          const mensaje = `Tienes un turno próximo el ${this.datePipe.transform(alert.turn.dateFrom, 'dd/MM/yyyy HH:mm')}.
+          Con ${alert.turn.schedule.supplier.user.lastName}, ${alert.turn.schedule.supplier.user.firstName}`;
 
-              const mensaje = `Tienes un turno próximo el ${this.datePipe.transform(alert.turn.dateFrom, 'dd/MM/yyyy HH:mm')}.`;
-
-              // Agregar mensaje utilizando el servicio MessageService de PrimeNG
-              this.messageService.add({
-                severity: 'info',
-                summary: 'Turno Próximo',
-                detail: mensaje
-              });
-            }
-          }
+          // Agregar mensaje utilizando el servicio MessageService de PrimeNG
+          this.messageService.add({
+            severity: 'info',
+            summary: 'Turno Próximo',
+            detail: mensaje
+          });
         }
       }
     });
-
   }
-
   realizarLlamadasAPISupplier() {
 
     this.notificacionesService.getAlertsSupplier(this.supplierIdFound).subscribe(alerts => {
+
       for (const alert of alerts) {
-        console.log('alertas: ', alerts);
-        
         // Verificar si el cliente de la alerta coincide con el proveedor del usuario
+        // Verificar si el cliente de la alerta coincide con el cliente del usuario
         if (alert.turn.schedule.supplier.id === this.supplierIdFound) {
-          for (const alert of alerts) {
-            console.log('alertas2: ', alerts);
-            // Verificar si el cliente de la alerta coincide con el cliente del usuario
-            if (alert.turn.schedule.supplier.id === this.supplierIdFound) {
-              console.log('ID del cliente de la alerta:', alert.turn.client.id);
-              console.log('ID de la alerta:', alert.id);
-              console.log('Nombre de la alerta:', alert.name);
-              console.log('Descripción de la alerta:', alert.description);
-              console.log('Fecha de inicio del turno:', alert.turn.dateFrom);
-              console.log('Fecha de fin del turno:', alert.turn.dateTo);
-
-              // ... Mostrar otras propiedades según la estructura de tu objeto alerta ...
-              const nombreAgenda = alert.turn.schedule.name;
-
-              const mensaje = `Tienes un turno próximo el ${this.datePipe.transform(alert.turn.dateFrom, 'dd/MM/yyyy HH:mm')}, en la agenda ${nombreAgenda}`;
-
-              // Agregar mensaje utilizando el servicio MessageService de PrimeNG
-              this.messageService.add({
-                severity: 'info',
-                summary: 'Turno Próximo',
-                detail: mensaje
-              });
-            }
+          // ... Mostrar otras propiedades según la estructura de tu objeto alerta ...
+          const nombreAgenda = alert.turn.schedule.name;
+          this.fecha = this.datePipe.transform(alert.turn.dateFrom, 'dd/MM/yyyy HH:mm')
+          if (!alert.turn.schedule.hasSign) {
+            const mensaje = `Tienes un turno próximo el ${this.fecha}, en la agenda ${nombreAgenda}.
+            Con el/la cliente ${alert.turn.client?.user?.lastName}, ${alert.turn.client?.user?.firstName}`;
+            this.customMessage = mensaje
+          } else {
+            const mensaje = `Tienes un turno próximo el ${this.fecha}
+            Con el/la cliente ${alert.turn.client?.user?.lastName}, ${alert.turn.client?.user?.firstName}`;
+            this.customMessage = mensaje
           }
         }
       }
     });
   }
 
-
-
-  esTurnoProximo(alert: any): boolean {
-    // Obtiene la fecha actual
-    const now = new Date();
-
-    // Obtiene la fecha del turno y la convierte a un objeto Date
-    const fechaTurno = new Date(alert.turn.dateFrom);
-
-    // Calcula la diferencia en milisegundos entre la fecha del turno y la fecha actual
-    const diferenciaMilisegundos = fechaTurno.getTime() - now.getTime();
-
-    // Convierte la diferencia de milisegundos a horas
-    const diferenciaHoras = diferenciaMilisegundos / (1000 * 3600);
-
-    // Verifica si la diferencia de horas es menor que 48
-
-    // if (diferenciaHoras < 48) {
-
-    console.log('`Tienes un turno próximo el')
-    // Muestra un mensaje de notificación utilizando PrimeNG MessageService
-    this.messageService.add({
-      severity: 'info',
-      summary: 'Turno Próximo',
-      detail: `Tienes un turno próximo el  'dd/MM/yyyy HH:mm')}.`
-    });
-
-    return true;
-    // }
+  aprobarSena(turno: any) {
+    // Lógica para aprobar la seña
+    // ...
+    console.log('Seña aprobada para el turno:', turno.id);
+    this.hideCustomMessage();
   }
+
+  onButtonClick() {
+    this.buttonClick.emit();
+  }
+
+  onButtonClickClose() {
+    this.visible = false;
+  }
+
+  private showCustomMessage(content: string, turno: any) {
+    this.customMessageContent = content;
+    this.showCustomButton = true;
+    this.currentTurno = turno;
+  }
+
+  private hideCustomMessage() {
+    this.showCustomButton = false;
+    this.customMessageContent = '';
+  }
+
 }
