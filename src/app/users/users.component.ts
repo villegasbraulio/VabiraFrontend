26f43a80f039681c7bfbaaad52ef35297e7cb857
Francisco Arana <francisco.arana@segurossura.com.ar>
2024-02-05 01:07:39 -0300

    update

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,6 +6,7 @@ import { UserModalComponent } from './users-modal.component';
 import { UserService } from './users.service';
 import { MessageService } from 'primeng/api';
 import { EditarAccesosModalComponent } from './editar-accesos-modal.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -23,7 +24,8 @@ export class UsersComponent implements OnInit {
   constructor(
     private userService: UserService,
     private dialog: MatDialog,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private router: Router
   ) {
     this.columnas = [
       { field: 'id', header: 'ID' },
@@ -142,6 +144,9 @@ export class UsersComponent implements OnInit {
       if (result) {
         this.usuarios = this.usuarios.map(u => (u.id === result.id ? result : u));
         this.messageService.add({severity: 'success', summary: 'Éxito', detail: 'Accesos del usuario actualizados correctamente.'});
+        
+        // Navegar a la misma página después de cerrar el diálogo
+        window.location.reload()
       }
     });
   }
